379736517d1d974da12b88910ff32c82aca80576
gabalja <gabgitalja2166@gmail.com>
2024-03-13 13:35:39 +0900

    feat: *로 변경

--- backend/src/main/java/com/ssafy/backend/config/WebConfig.java
+++ backend/src/main/java/com/ssafy/backend/config/WebConfig.java
@@ -7,12 +7,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {    @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
-
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedOrigins("http://nocolored.store")
-                .allowedOrigins("http://nocolored.store:18080")
-                .allowedOrigins("http://nocolored.store:8080");
-//                .allowedOrigins("https://i10a709.p.ssafy.io");
+                .allowedOrigins("*");
+    //                .allowedOrigins("https://i10a709.p.ssafy.io");
+
+//        corsRegistry.addMapping("/**")
+//                .allowedOrigins("http://localhost:5173")
+//                .allowedOrigins("http://nocolored.store")
+//                .allowedOrigins("http://nocolored.store:18080")
+//                .allowedOrigins("http://nocolored.store:8080");
+////                .allowedOrigins("https://i10a709.p.ssafy.io");
     }
 }
