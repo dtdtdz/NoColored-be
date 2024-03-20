00b220c03018b43f8848ddedc58bbe251cf8d49c
dtdtdz <dtdtdz@gmail.com>
2024-03-20 09:33:47 +0900

    refact: game controller to ready

--- backend/src/main/java/com/ssafy/backend/game/controller/GameController.java
+++ backend/src/main/java/com/ssafy/backend/game/controller/GameController.java
@@ -6,11 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/game")
+@RequestMapping("/ready")
 public class GameController {
     @GetMapping
     private ResponseEntity<String> ready(){
-
         return ResponseEntity.ok("확인");
     }
 }
