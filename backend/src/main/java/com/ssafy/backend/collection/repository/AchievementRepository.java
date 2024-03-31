329a9846da4c00984cccc9b73806a3d68f2c9868
gabalja <gabgitalja2166@gmail.com>
2024-03-31 09:51:32 +0900

    feat: 칭호, 업적 수정 후 유저 회원가입에도 반영

--- backend/src/main/java/com/ssafy/backend/collection/repository/AchievementRepository.java
+++ backend/src/main/java/com/ssafy/backend/collection/repository/AchievementRepository.java
@@ -1,6 +1,6 @@
 package com.ssafy.backend.collection.repository;
 
-import com.ssafy.backend.collection.dao.Achievement;
+import com.ssafy.backend.collection.document.Achievement;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface AchievementRepository extends MongoRepository<Achievement,Integer> {
--- backend/src/main/java/com/ssafy/backend/collection/repository/UserCollectionRepository.java
+++ backend/src/main/java/com/ssafy/backend/collection/repository/UserCollectionRepository.java
@@ -1,6 +1,6 @@
 package com.ssafy.backend.collection.repository;
 
-import com.ssafy.backend.collection.dao.UserCollection;
+import com.ssafy.backend.collection.document.UserCollection;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserCollectionRepository extends MongoRepository<UserCollection,String> {
